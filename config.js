091d337016c499d1c70bfd5823a79de25428bfe6
ilyasslwa3r <70577138+ilyasslwa3r@users.noreply.github.com>
2020-09-30 17:15:52 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NzQ3NTU4ODU1ODc3MTk3ODM1.X0QocA.wfLtbnsfUjzj9f3RCp9tvcUptAY', /// توكن البوت
+    YT_API_KEY: 'api ', /// مش مهم
+    prefix: '/', // هنا برفكس او مقدمة البوت
+    devs: ['421711597141229569'] //// ايدي حسابك
+}
