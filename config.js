661c4a762e7a261e8393ecf3627dda6f64983d1e
ilyasslwa3r <70577138+ilyasslwa3r@users.noreply.github.com>
2020-10-08 20:47:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: 'NzQ3NTU4ODU1ODc3MTk3ODM1.X0QocA.wfLtbnsfUjzj9f3RCp9tvcUptAY', /// توكن البوت
+    TOKEN: '', /// توكن البوت
     YT_API_KEY: 'api ', /// مش مهم
     prefix: '/', // هنا برفكس او مقدمة البوت
     devs: ['421711597141229569'] //// ايدي حسابك
